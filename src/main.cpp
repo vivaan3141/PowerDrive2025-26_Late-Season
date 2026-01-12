5f9908e1d47b2b338e8a6b77d2872fe7a7407f69
Vivaan Gupta <vivaangupta3141@gmail.com>
2026-01-11 17:36:47 -0800

    Controls placed in seperate file - controls.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "vex.h"
 #include "robot-config.h"
 #include "functions.h"
+#include "controls.cpp"
+
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -13,8 +15,6 @@
 using namespace vex;
 competition Competition;
 
-bool stateLoader=false;
-bool stateDescore=false;
 
 double distance(){ return gap.objectDistance(inches); }
 
@@ -198,107 +198,107 @@ void autonomous() {
   autonCodes(8);
 }
  
-void reverseIntake(){
-  low.spin(reverse);
-  high.spin(reverse);
-}
+// void reverseIntake(){
+//   low.spin(reverse);
+//   high.spin(reverse);
+// }
 
-void middleGoal(){
-  low.spin(forward);
-  high.spin(reverse);
-  Controller.Screen.print("Middle Goal");
-}
+// void middleGoal(){
+//   low.spin(forward);
+//   high.spin(reverse);
+//   Controller.Screen.print("Middle Goal");
+// }
 
-void intake(){
-  low.spin(forward);
-  Controller.Screen.print("Intake");
-}
+// void intake(){
+//   low.spin(forward);
+//   Controller.Screen.print("Intake");
+// }
 
-void longGoal(){
-  Controller.Screen.clearLine();
+// void longGoal(){
+//   Controller.Screen.clearLine();
 
-  high.spin(forward); // Score long, moves all stages
-  low.spin(forward);
+//   high.spin(forward); // Score long, moves all stages
+//   low.spin(forward);
 
-  Controller.Screen.print("LONG GOAL");
-}
-void loadOut(){
-  Controller.Screen.clearLine();
+//   Controller.Screen.print("LONG GOAL");
+// }
+// void loadOut(){
+//   Controller.Screen.clearLine();
 
-  Controller.Screen.print("Loading"); 
-  Loader.set(true);
-}
+//   Controller.Screen.print("Loading"); 
+//   Loader.set(true);
+// }
 
-void loadRest(){
-  Controller.Screen.clearLine();
-  Loader.set(false);
-}
+// void loadRest(){
+//   Controller.Screen.clearLine();
+//   Loader.set(false);
+// }
 
-void descoreOut(){
-  Controller.Screen.clearLine();
+// void descoreOut(){
+//   Controller.Screen.clearLine();
 
-  Descore.set(true);
-}
+//   Descore.set(true);
+// }
 
-void descoreIn(){
-  Controller.Screen.clearLine();
+// void descoreIn(){
+//   Controller.Screen.clearLine();
 
-  Descore.set(false);
-}
-void reverseIntakeRELEASED(){
-  Controller.Screen.clearLine();
+//   Descore.set(false);
+// }
+// void reverseIntakeRELEASED(){
+//   Controller.Screen.clearLine();
 
-  low.stop();
-  high.stop();
-}
+//   low.stop();
+//   high.stop();
+// }
 
-void middleGoalRELEASED(){
-  Controller.Screen.clearLine();
-  low.stop();
-  high.stop();
-}
+// void middleGoalRELEASED(){
+//   Controller.Screen.clearLine();
+//   low.stop();
+//   high.stop();
+// }
 
-void longGoalRELEASED(){
-  Controller.Screen.clearLine();
+// void longGoalRELEASED(){
+//   Controller.Screen.clearLine();
 
-  high.stop();
-  low.stop();
-}
+//   high.stop();
+//   low.stop();
+// }
 
-void intakeRELEASED(){
-  Controller.Screen.clearLine();
+// void intakeRELEASED(){
+//   Controller.Screen.clearLine();
 
-  high.stop();
-  low.stop();
-}
+//   high.stop();
+//   low.stop();
+// }
 
 
-void load(){
-  if (stateLoader==true){
-    loadOut();
-    stateLoader=false; // Happy :)
-  }else {
-    loadRest();
-    stateLoader=true;
-  }
-}
+// void load(){
+//   if (stateLoader==true){
+//     loadOut();
+//     stateLoader=false; // Happy :)
+//   }else {
+//     loadRest();
+//     stateLoader=true;
+//   }
+// }
 
-void descore(){
-  if (stateDescore==true){
-    descoreOut();
-    stateDescore=false; // Happy :)
-  }else {
-    descoreIn();
-    stateDescore=true;
-  }
-}
+// void descore(){
+//   if (stateDescore==true){
+//     descoreOut();
+//     stateDescore=false; // Happy :)
+//   }else {
+//     descoreIn();
+//     stateDescore=true;
+//   }
+// }
 
-void descoreTest(){
-  Descore.set(true);
-  wait(1,sec);
-    Descore.set(false);
+// void descoreTest(){
+//   Descore.set(true);
+//   wait(1,sec);
+//     Descore.set(false);
 
-}
+// }
 
 
 
